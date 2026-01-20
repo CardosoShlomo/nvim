cae8569e386d4025499feb0d344938f664ca286c
CardosoShlomo <cardososhlomo@gmail.com>
2026-01-20 23:44:32 +0200

    Add flutter-tools log filter and layout

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -6,13 +6,24 @@ return {
         'stevearc/dressing.nvim',
     },
     opts = {
+      dev_log = {
+        enabled = true,
+        open_cmd = "botright 70vnew",
+        filter = function(log_line)
+          if not log_line then return true end
+          return log_line:match("flutter")
+              or log_line:match("^Launching")
+              or log_line:match("^Running")
+              or log_line:match("^Syncing")
+              or log_line:match("^Restarted")
+              or log_line:match("^Error")
+              or log_line:match("^Exception")
+        end,
+      },
       lsp = {
         settings = {
-          -- Include external packages in analysis
           analysisExcludedFolders = {},
-          -- Better completion
           completeFunctionCalls = true,
-          -- Show SDK source in go-to-definition
           includeDependenciesInWorkspaceSymbols = true,
         },
       },
